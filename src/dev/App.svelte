74af11e6c2d5198c4b4657752f1ef779d2a4391b
jonasjs <joonasjss@gmail.com>
2020-05-12 17:03:29 -0300

    feat: 🎸 initial structure

---
+++ src/dev/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import AdressAutocomplete from "../lib/index.svelte";
+</script>
+
+
+<main>
+  
+  <AdressAutocomplete>
+
+    <input slot="cep"/>
+
+  </AdressAutocomplete>
+
+</main>
+
+
+<style>
+  main {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+</style>
---
+++ src/lib/index.svelte
@@ -0,0 +1,14 @@
+<script>
+
+  export let cepValue;
+
+</script>
+
+<div>
+	<slot cep="cep">
+    <input bind:value={cepValue}>
+  </slot>
+  
+</div>
+
+<style></style>
